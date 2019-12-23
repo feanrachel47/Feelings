b8ad7da420134e82bab76441571b0a0b1fe23600
rwyy <feanrachel@gmail.com>
2019-12-23 12:04:06 +0800

    initial

---
+++ app/src/main/java/com/example/feelings/FeelingViewModel.kt
@@ -0,0 +1,8 @@
+package com.example.feelings
+
+import android.app.Application
+
+class FeelingViewModel(application: Application):AndroidViewModel(application) {
+    private val repository: UserRepository
+
+}
--- app/src/main/java/com/example/feelings/MainActivity.kt
+++ app/src/main/java/com/example/feelings/MainActivity.kt
@@ -1,12 +1,25 @@
 package com.example.feelings
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        fab.setOnClickListener{
+            val intent: Intent = Intent( this, AddActivity::class.java)
+
+        }
     }
+
+
+    companion object{
+        val REQUEST_CODE = 1
+    }
+
 }
